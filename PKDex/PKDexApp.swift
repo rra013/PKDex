302a1063d1b7d2698e4d2372c94ff42fabf21d49
Rishi Anand <rishianandblack@gmail.com>
2026-04-13 11:02:32 -0400

    Initial Commit

---
+++ PKDex/PKDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PKDexApp.swift
+//  PKDex
+//
+//  Created by Rishi Anand on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct PKDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
